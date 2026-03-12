ed0b271e38c3968592870e0a09670ed307610495
Hassan-Abdi-89 <hassanmahat89@proton.me>
2026-03-12 16:29:15 -0400

    modify variables.tf

---
+++ Terraform/WordPress-Terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region"{
+    default = "us-east-1"
+}
+variable "instance_type"{
+    default = "t2.micro"
+}
+variable "key_name"{
+    description = "SSH key pair name"
+}
+variable "allowed_ip"{
+    description = ""
+}
